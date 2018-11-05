9615ba9d54ab1065196f6b83092f6abaf2eb681f
PatyPrz <1717110752@utectulancingo.edu.mx>
2018-11-04 20:36:50 -0600

    Se crearon objetos para los constructores de cada modelo, vista y controlador y se agregaron al array de controladores para ser utilizados en el frame principal

--- Sistema/src/main/Main.java
+++ Sistema/src/main/Main.java
@@ -17,17 +17,37 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ModelProductos modelProductos = new ModelProductos();
-        ViewProductos viewProductos = new ViewProductos();
-        ControllerProductos controllerProductos = new ControllerProductos(modelProductos, viewProductos);
         
         ModelClientes modelClientes = new ModelClientes();
         ViewClientes viewClientes = new ViewClientes();
         ControllerClientes controllerClientes = new ControllerClientes(modelClientes, viewClientes);
         
+        ModelDirecciones modelDirecciones = new ModelDirecciones();
+        ViewDirecciones viewDirecciones = new ViewDirecciones();
+        ControllerDirecciones controllerDirecciones = new ControllerDirecciones(modelDirecciones, viewDirecciones);
+        
+        ModelEmpleados modelEmpleados = new ModelEmpleados();
+        ViewEmpleados viewEmpleados = new ViewEmpleados();
+        ControllerEmpleados controllerEmpleados = new ControllerEmpleados(modelEmpleados, viewEmpleados);
+        
+        ModelProductos modelProductos = new ModelProductos();
+        ViewProductos viewProductos = new ViewProductos();
+        ControllerProductos controllerProductos = new ControllerProductos(modelProductos, viewProductos);
+        
+        ModelProveedores modelProveedores = new ModelProveedores();
+        ViewProveedores viewProveedores = new ViewProveedores();
+        ControllerProveedores controllerProveedores = new ControllerProveedores(modelProveedores, viewProveedores);
+        
+        ModelUsuarios modelUsuarios = new ModelUsuarios();
+        ViewUsuarios viewUsuarios = new ViewUsuarios();
+        ControllerUsuarios controllerUsuarios = new ControllerUsuarios(modelUsuarios, viewUsuarios);
+        
         Object[] controllers = new Object[5];
-        controllers[0] = controllerProductos;
-        controllers[1] = controllerClientes;
+        controllers[0] = controllerClientes;
+        controllers[1] = controllerEmpleados;
+        controllers[2] = controllerProductos;
+        controllers[3] = controllerProveedores;
+        controllers[4] = controllerUsuarios;
         
         ModelPrincipal modelPrincipal = new ModelPrincipal();
         ViewPrincipal viewPrincipal = new ViewPrincipal();
