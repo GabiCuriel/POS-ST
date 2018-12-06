17c1bbe111639c669082a8dcdefe8e06a838c306
PatyPrz <1717110752@utectulancingo.edu.mx>
2018-12-05 18:40:19 -0600

    Se programo el main para lograr interactuar correctamente con el login

--- Sistema/src/main/Main.java
+++ Sistema/src/main/Main.java
@@ -12,76 +12,23 @@ import controllers.*;
  * @author Salvador Hernández Mendoza
  */
 public class Main {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        ModelClientes modelClientes = new ModelClientes();
-        ViewClientes viewClientes = new ViewClientes();
-        ControllerClientes controllerClientes = new ControllerClientes(modelClientes, viewClientes);
-        
-        ModelClientes_CRUD modelClientes_CRUD = new ModelClientes_CRUD();
-        ViewClientes_CRUD viewClientes_CRUD = new ViewClientes_CRUD();
-        ControllerClientes_CRUD controllerClientes_CRUD = new ControllerClientes_CRUD(modelClientes_CRUD, viewClientes_CRUD);
-        
-        ModelEmpleados modelEmpleados = new ModelEmpleados();
-        ViewEmpleados viewEmpleados = new ViewEmpleados();
-        ControllerEmpleados controllerEmpleados = new ControllerEmpleados(modelEmpleados, viewEmpleados);
-        
-        ModelEmpleados_CRUD modelEmpleados_CRUD = new ModelEmpleados_CRUD();
-        ViewEmpleados_CRUD viewEmpleados_CRUD = new ViewEmpleados_CRUD();
-        ControllerEmpleados_CRUD controllerEmpleados_CRUD = new ControllerEmpleados_CRUD(modelEmpleados_CRUD, viewEmpleados_CRUD);
-        
-        ModelProductos modelProductos = new ModelProductos();
-        ViewProductos viewProductos = new ViewProductos();
-        ControllerProductos controllerProductos = new ControllerProductos(modelProductos, viewProductos);
-        
-        ModelProductos_CRUD modelProductos_CRUD = new ModelProductos_CRUD();
-        ViewProductos_CRUD viewProductos_CRUD = new ViewProductos_CRUD();
-        ControllerProductos_CRUD controllerProductos_CRUD = new ControllerProductos_CRUD(modelProductos_CRUD, viewProductos_CRUD);
-        
-        ModelProveedores modelProveedores = new ModelProveedores();
-        ViewProveedores viewProveedores = new ViewProveedores();
-        ControllerProveedores controllerProveedores = new ControllerProveedores(modelProveedores, viewProveedores);
-        
-        ModelProveedores_CRUD modelProveedores_CRUD = new ModelProveedores_CRUD();
-        ViewProveedores_CRUD viewProveedores_CRUD = new ViewProveedores_CRUD();
-        ControllerProveedores_CRUD controllerProveedores_CRUD = new ControllerProveedores_CRUD(modelProveedores_CRUD, viewProveedores_CRUD);
-        
-        //ModelSucursales modelSucursales = new ModelSucursales();
-        //ViewSucursales viewSucursales = new ViewSucursales();
-        //ControllerSucursales controllerSucursales = new ControllerSucursales(modelSucursales, viewSucursales);
-        
-        //ModelSucursales_CRUD modelSucursales_CRUD = new ModelSucursales_CRUD();
-        //ViewSucursales_CRUD viewSucursales_CRUD = new ViewSucursales_CRUD();
-        //ControllerSucursales_CRUD controllerSucursales_CRUD = new ControllerSucursales_CRUD(modelSucursales_CRUD, viewSucursales_CRUD);
-        
-        ModelUsuarios modelUsuarios = new ModelUsuarios();
-        ViewUsuarios viewUsuarios = new ViewUsuarios();
-        ControllerUsuarios controllerUsuarios = new ControllerUsuarios(modelUsuarios, viewUsuarios);
-                
-        ModelUsuarios_CRUD modelUsuarios_CRUD = new ModelUsuarios_CRUD();
-        ViewUsuarios_CRUD viewUsuarios_CRUD = new ViewUsuarios_CRUD();
-        ControllerUsuarios_CRUD controllerUsuarios_CRUD = new ControllerUsuarios_CRUD(modelUsuarios_CRUD, viewUsuarios_CRUD);
-        
-        Object[] controllers = new Object[10];
-        controllers[0] = controllerClientes;
-        controllers[1] = controllerClientes_CRUD;
-        controllers[2] = controllerEmpleados;
-        controllers[3] = controllerEmpleados_CRUD;
-        controllers[4] = controllerProductos;
-        controllers[5] = controllerProductos_CRUD;
-        controllers[6] = controllerProveedores;
-        controllers[7] = controllerProveedores_CRUD;
-        controllers[8] = controllerUsuarios;
-        controllers[9] = controllerUsuarios_CRUD;
-        
-        
-        ModelPrincipal modelPrincipal = new ModelPrincipal();
-        ViewPrincipal viewPrincipal = new ViewPrincipal();
-        ControllerPrincipal controllerPrincipal = new ControllerPrincipal(modelPrincipal, viewPrincipal, controllers);
+        ModelLogueo modelLogueo = new ModelLogueo();
+        ViewLogueo viewLogueo = new ViewLogueo();
+        ControllerLogueo controllerLogueo = new ControllerLogueo(modelLogueo, viewLogueo);
+        System.out.println(modelLogueo.isValue());
+        while (true){
+            System.out.println("#####");
+            if (modelLogueo.isValue() == true){
+                System.out.println(modelLogueo.isValue());
+                Constructores constructores = new Constructores();
+                constructores.Constructores();
+                viewLogueo.dispose();
+                break;
+            }
+        }
     }
-    
 }
